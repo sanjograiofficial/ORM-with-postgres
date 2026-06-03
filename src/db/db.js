3470fb8bf228edd59143ddeabeba73ca46619de1
sanjograiofficial <sanjograiofficial@gmail.com>
2026-06-03 14:58:07 +0545

    added exception handlers

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,10 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const connectionString = `${process.env.DATABASE_URL}`;
 
-let prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma ;
--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -1,60 +1,121 @@
 import prisma from "../db/db.js";
 
 const getAllStudents = async (req, res) => {
-  let allStudents = prisma.students.findMany();
-  res.json({
-    message: "All students found",
-    data: allStudents,
-  });
+  try {
+    let allStudents = await prisma.students.findMany();
+    if (allStudents.length == 0) {
+      return res.status(404).json({
+        message: "No students found",
+      });
+    }
+    res.json({
+      message: "All students found",
+      data: allStudents,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      message: "Failed to fetch students",
+      error: e,
+    });
+  }
 };
 const getStudentById = async (req, res) => {
-  let matchStudent = await prisma.students.findUnique({
-    where: {
-      id: Number(req.params.id),
-    },
-  });
-  res.status(200).json({
-    message: "Student found",
-    data: matchStudent,
-  });
+  try {
+    let matchStudent = await prisma.students.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    if (!matchStudent) {
+      return res.status(404).json({
+        message: "No student found with that id",
+      });
+    }
+    res.status(200).json({
+      message: "Student found",
+      data: matchStudent,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      message: "Failed to fetch student",
+    });
+  }
 };
 const createStudent = async (req, res) => {
-  let data = req.body;
-  let createdStudent = await prisma.students.create({
-    data: data,
-  });
-  res.status(200).json({
-    message: "Student created successfully",
-    data: createdStudent,
-  });
+  try {
+    let data = req.body;
+    let createdStudent = await prisma.students.create({
+      data: data,
+    });
+    res.status(201).json({
+      message: "Student created successfully",
+      data: createdStudent,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      message: "Faile to create student",
+    });
+  }
 };
 const updateStudent = async (req, res) => {
-  let data = req.body;
-  let { name, email } = req.body;
-  let updatedStudent = await prisma.students.update({
-    where: {
-      id: Number(req.params.id),
-    },
-    data: {
-      name,
-      email,
-    },
-  });
-  res.status(200).json({
-    message: "Student updated successfully",
-    data: updatedStudent,
-  });
+  try {
+    let data = req.body;
+    let { name, email } = req.body;
+    let updatedStudent = await prisma.students.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name,
+        email,
+      },
+    });
+    if (updatedStudent) {
+      return res.status(404).json({
+        message: "No student found with that id",
+      });
+    }
+    res.status(200).json({
+      message: "Student updated successfully",
+      data: updatedStudent,
+    });
+  } catch (e) {
+    if (e.code === "P2025") {
+      return res.status(404).json({
+        message: "No student found with that id",
+      });
+    }
+    console.log(e);
+    return res.json({
+      message: "Failed to update user",
+    });
+  }
 };
 const deleteStudent = async (req, res) => {
-  let deletedStudent = await prisma.students.delete({
-    where: {
-      id: Number(req.params.id),
-    },
-  });
-  res.status(200).json({
-    message: "Student deleted successfully",
-    data: deletedStudent,
-  });
+  try {
+    let deletedStudent = await prisma.students.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json({
+      message: "Student deleted successfully",
+      data: deletedStudent,
+    });
+  } catch (e) {
+    if (e.code === "P2025") {
+      return res.status(404).json({
+        message: "No student found with that id",
+      });
+    }
+    console.log(e);
+    return res.json({
+      message: "Failed to delete student",
+    });
+  }
 };
 
 export {
