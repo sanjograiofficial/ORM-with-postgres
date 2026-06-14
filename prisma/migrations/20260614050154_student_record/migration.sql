44d981c180e99289fd47efd113a372561555bace
sanjograiofficial <sanjograiofficial@gmail.com>
2026-06-14 11:06:45 +0545

    added user and product

--- prisma/migrations/20260614050154_student_record/migration.sql
+++ prisma/migrations/20260614050154_student_record/migration.sql
@@ -57,6 +57,28 @@ CREATE TABLE "enrollments" (
     CONSTRAINT "enrollments_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "students_email_key" ON "students"("email");
 
@@ -65,6 +87,9 @@ CREATE UNIQUE INDEX "department_name_key" ON "department"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "teacher_email_key" ON "teacher"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "students" ADD CONSTRAINT "students_department_id_fkey" FOREIGN KEY ("department_id") REFERENCES "department"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
